0bf1e42bad32489a131cbe9b6843983a159cbc8b
Vikas5686 <91067260+Vikas5686@users.noreply.github.com>
2023-04-25 19:41:17 +0530

    error 3

--- src/Conponents/Home.js
+++ src/Conponents/Home.js
@@ -3,15 +3,12 @@ function Home() {
   return (
     <div className="row_2">
     <h1>Home</h1>
-<<<<<<< HEAD
     <h1>Home3sffff</h1>
     <h1>Home3sffff</h1>
     <h1>Home3sffff</h1>
     <h1>Home3sffff</h1>
     <h1>Home3sffff</h1>
     <h1>Home3sffff</h1>
-=======
->>>>>>> parent of 9f20bd6 (error)
     </div>
   );
 }
